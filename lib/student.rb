dfad9d640752d3f85632dbef3f296f15f92d0320
github username <github email address>
2017-06-14 18:47:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,43 @@
+require "pry"
+
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    );"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students;"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?);", [self.name, self.grade])
+    sql = "SELECT id FROM students ORDER BY id DESC LIMIT 1;"
+    @id = DB[:conn].execute(sql).flatten[0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
